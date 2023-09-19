00fbcf040d07389c107576638c7fc8cf2a4743b5
Octavian Sulic <octavius1992@gmail.com>
2023-09-19 16:30:48 +0300

    Add bookinstance controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -17,26 +17,26 @@ exports.author_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle Author create POST.
-exports.author_create_post = asyncHandler(async(req, res, next)=> {
+exports.author_create_post = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Author create POST");
 });
 
 // Display Author delete from on GET.
-exports.author_delete_get = asyncHandler(async(req, res, next) =>{
+exports.author_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Author delete GET");
 });
 
 // Handle Author delete on POST.
-exports.author_delete_post = asyncHandler(async(req, res, next)=>{
+exports.author_delete_post = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Author delete POST");
 });
 
 // Display Author update on GET.
-exports.author_update_get = asyncHandler(async(req, res, next)=>{
+exports.author_update_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Author update GET")
 })
 
 // Handle Author update on POST.
-exports.author_update_post = asyncHandler(async(req, res, next)=>{
+exports.author_update_post = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Author update POST")
 })
