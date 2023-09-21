149f0610abaf86843219d8d1889b81bfa1b590f2
Octavian Sulic <octavius1992@gmail.com>
2023-09-21 11:34:14 +0300

    Add authorController list action and view, add virtual prop to author model.

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const {DateTime} = require("luxon")
 
 const Schema = mongoose.Schema;
 
@@ -23,6 +24,16 @@ AuthorSchema.virtual("name").get(function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
+});
+
+// Virtual for bookinstance formatted due back date
+AuthorSchema.virtual("lifespan").get(function () {
+  if (!this.date_of_birth) return ""
+  else {
+    const dob = DateTime.fromJSDate(this.date_of_birth).toFormat('dd LLL yyyy');
+    const dod = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toFormat('dd LLL yyyy') : "";
+    return `${dob} - ${dod}`
+  }
 });
 
 // Export model
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -20,6 +20,7 @@ BookInstanceSchema.virtual("url").get(function() {
   return `/catalog/bookinstance/${this._id}`;
 });
 
+// Virtual for bookinstance formatted due back date
 BookInstanceSchema.virtual("due_back_formatted").get(function(){
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
