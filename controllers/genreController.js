56356a97e48e9ba24f8e21dd587f50c66b793092
Octavian Sulic <octavius1992@gmail.com>
2023-09-21 12:18:18 +0300

    Add genreController list action and view, update stylesheet.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -3,7 +3,13 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenres = await Genre.find().sort({name: 1}).exec();
+  res.render("genre_list", {
+    title: "Genre List",
+    genre_list: allGenres,
+    head: "head",
+    sidebar: "sidebar",
+  })
 });
 
 // Display detail page for a specific Genre.
