7da39a492d31b1152feb1ce92f9547a2ceddc566
Octavian Sulic <octavius1992@gmail.com>
2023-09-22 16:42:17 +0300

    Add authorController create actions for get&post methods, update genre title render.

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -40,7 +40,7 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res, next) => {
   res.render("genre_form", {
-    title: "Create Genre",
+    title: "Add Genre",
     head: "head",
     sidebar: "sidebar",
   });
@@ -65,10 +65,9 @@ exports.genre_create_post = [
     if (!errors.isEmpty()) {
       // There are errors. Render the form again with sanitized values/error messages.
       res.render("genre_form", {
-        title: "Create Genre",
+        title: "Add Genre",
         genre: genre,
         errors: errors.array(),
-        title: "Create Genre",
         head: "head",
         sidebar: "sidebar", 
       });
